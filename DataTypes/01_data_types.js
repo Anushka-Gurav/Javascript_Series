cd6eed0fc4278f41d6f17bcc6930cd38be30b7e8
Anushka Gurav <162349811+Anushka-Gurav@users.noreply.github.com>
2026-01-18 17:11:50 +0000

    data_push

---
+++ DataTypes/01_data_types.js
@@ -0,0 +1,31 @@
+let name="Anushka"
+let age=21
+let isLoggedIn = false
+
+/*
+number =>2
+bigint 
+string =>""
+boolean => true/false
+null => standalone type
+undefined => when nothing is initialized
+symbol => uniqueness
+*/
+console.log(typeof name)
+
+
+
+let num="33"
+let val=Number(num)
+console.log(typeof val)
+console.log(typeof num)
+/*
+when try to convert string to number
+below represent value in val variable
+*/
+// "33" =>33
+// "33abc" => NaN
+// null => 0
+// true => 1
+// false =>0
+
---
+++ DataTypes/data_conversion.js
@@ -0,0 +1,17 @@
+console.log("1"+2)
+console.log(1+"2")
+console.log("1"+2+2)
+console.log(1+2+"2")
+console.log((3 + 4) * 5 % 3)
+console.log(+true)
+
+/*
+Primitives
+
+7 types :
+String, Number, Boolean, null, undefined, Symbol, BigInt
+
+Reference Type (Non Primitive)
+Array, Object, Functions
+
+*/
